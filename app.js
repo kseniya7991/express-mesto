dfe123695eacc340761687832cccc148fb9941ab
Kseniya St <kseniya7991@gmail.com>
2021-07-08 19:58:05 +0300

    add CORS

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use((req, res, next) => {
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.status(200);
+    return;
   }
 
   next();
