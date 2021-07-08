7778a2f7f1e8ba7ee6e7e84390cdaa95340ca368
Kseniya St <kseniya7991@gmail.com>
2021-07-08 07:04:18 +0300

    add CORS

--- app.js
+++ app.js
@@ -15,13 +15,37 @@ const { createUser, login } = require('./controllers/user');
 
 const app = express();
 
-const method = (value) => {
+const methodValidation = (value) => {
   const correctLink = validator.isURL(value, { require_protocol: true });
   if (!correctLink) {
     return new BadRequest('Введены некорректные аватара пользователя');
   }
   return value;
 };
+
+const allowedCors = [
+  'https://kst.mesto.nomoredomains.club/',
+  'http://kst.mesto.nomoredomains.club/',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+  }
+  next();
+});
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,7 +65,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(method, 'Validation Link'),
+    avatar: Joi.string().custom(methodValidation, 'Validation Link'),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
