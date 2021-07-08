83364c0c84a34a63a52bfe9db4f253455ed40aa1
Kseniya St <kseniya7991@gmail.com>
2021-07-08 19:07:12 +0300

    add CORS

--- app.js
+++ app.js
@@ -30,11 +30,8 @@ const allowedCors = [
 ];
 
 app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
   const { method } = req;
+  const { origin } = req.headers;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   const requestHeaders = req.headers['access-control-request-headers'];
@@ -43,7 +40,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.status(200);
+  }
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   next();
