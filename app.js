ab1204d588949eb691742ec77409745d25192aae
Kseniya St <kseniya7991@gmail.com>
2021-07-09 06:51:20 +0300

    add CORS

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
     res.status(200);
   }
 
@@ -62,13 +61,6 @@ app.use(cookieParser('secret'));
 
 app.use(requestLogger); // подключаем логгер запросов
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
@@ -78,6 +70,13 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
   }),
 }), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 
 // авторизация
 app.use(auth);
